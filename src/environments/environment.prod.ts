9bd8b3ee3b3382c4b76bfb6f55284fc64d28c673
viejopool <fpvizuete@gmail.com>
2021-12-21 21:34:33 -0500

    cambios del enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   API:{URL:'https://v6.exchangerate-api.com/v6/',
-  key:'5b7e2e6ef9bda4b3841b6851'}
+  key:'XXXXXXXXX cambiar por su llave generada de https://www.exchangerate-api.com/'}
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   API:{URL:'https://v6.exchangerate-api.com/v6/',
-  key:'5b7e2e6ef9bda4b3841b6851'}
+  key:'XXXXXXXXX cambiar por su llave generada de https://www.exchangerate-api.com/'}
   };
 
 /*
